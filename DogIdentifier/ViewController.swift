0882c555a3f59f819b6c806e5d6c0542eb299752
shiguoFeng <shiguo@ualberta.ca>
2023-04-01 20:12:40 -0600

    Initial Commit

---
+++ DogIdentifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogIdentifier
+//
+//  Created by Shiguo Feng on 2023-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
