919debde011ecf68c1cf8596a0850da1d797c5c4
shiguoFeng <shiguo@ualberta.ca>
2023-04-13 23:44:22 -0600

    MVC model completed, testing for working?

---
+++ DogIdentifier/Model/DogBreed.swift
@@ -0,0 +1,33 @@
+//
+//  DogBreed.swift
+//  DogIdentifier
+//
+//  Created by Shiguo Feng on 2023-04-13.
+//
+
+import Foundation
+import UIKit
+
+struct DogBreed {
+    let name: String
+    let probability: Float
+    let description: String
+    let imageURL: String
+    
+    var probabilityString: String {
+        return String(format: "%.2f", probability * 100)
+    }
+    
+    var subtitleColor : UIColor {
+        switch probability {
+        case 0.85...1:
+            return UIColor.green
+        case 0.65 ... 0.84:
+            return UIColor.blue
+        case 0.3 ... 0.64:
+            return UIColor.gray
+        default:
+            return UIColor.red
+        }
+    }
+}
---
+++ DogIdentifier/Model/DogBreedManager.swift
@@ -0,0 +1,114 @@
+//
+//  DogBreedManager.swift
+//  DogIdentifier
+//
+//  Created by Shiguo Feng on 2023-04-13.
+//
+
+import Foundation
+import CoreML
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+protocol DogBreedManagerDelegate {
+    func updataDisplay(_ DogBreedManager: DogBreedManager, DogBreed: DogBreed)
+}
+struct DogBreedManager {
+    
+    let uniqueLabels = ["Affenpinscher", "Afghan Hound", "African Hunting Dog", "Airedale Terrier", "American Staffordshire Terrier", "Appenzeller Sennenhund", "Australian Terrier", "Basenji", "Basset", "Beagle", "Bedlington Terrier", "Bernese Mountain Dog", "Black-and-tan Coonhound", "Blenheim Spaniel", "Bloodhound", "Bluetick", "Border Collie", "Border Terrier", "Borzoi", "Boston Terrier", "Bouvier Des Flandres", "Boxer (dog)", "Brabancon Griffon", "Briard", "Brittany Spaniel", "Bull Mastiff", "Cairn Terrier", "Cardigan Welsh Corgi", "Chesapeake Bay Retriever", "Chihuahua (dog)", "Chow (dog)", "Clumber Spaniel", "Cocker Spaniel", "Collie", "Curly-coated Retriever", "Dandie Dinmont", "Dhole", "Dingo", "Doberman", "English Foxhound", "English Setter", "English Springer", "Entlebucher", "American Eskimo Dog", "Flat-coated Retriever", "French Bulldog", "German Shepherd", "German Short-haired Pointer", "Giant Schnauzer", "Golden Retriever", "Gordon Setter", "Great Dane", "Great Pyrenees", "Greater Swiss Mountain Dog", "Groenendael", "Ibizan Hound", "Irish Setter", "Irish Terrier", "Irish Water Spaniel", "Irish Wolfhound", "Italian Greyhound", "Japanese Spaniel", "Keeshond", "Australian Kelpie", "Kerry Blue Terrier", "Komondor", "Kuvasz", "Labrador Retriever", "Lakeland Terrier", "Leonberger", "Lhasa Apso", "Malamute", "Belgian Shepherd", "Maltese Dog", "Xoloitzcuintle", "Miniature Pinscher", "Miniature Poodle", "Miniature Schnauzer", "Newfoundland dog", "Norfolk Terrier", "Norwegian Elkhound", "Norwich Terrier", "Old English Sheepdog", "Otterhound", "Papillon dog", "Pekinese", "Pembroke Welsh Corgi", "Pomeranian dog", "Pug", "Redbone Coonhound", "Rhodesian Ridgeback", "Rottweiler", "St. Bernard (dog)", "Saluki", "Samoyed dog", "Schipperke", "Scotch Terrier", "Scottish Deerhound", "Sealyham Terrier", "Shetland Sheepdog", "Shih-tzu", "Siberian Husky", "Silky Terrier", "Soft-coated Wheaten Terrier", "Staffordshire Bull Terrier", "Standard Poodle", "Standard Schnauzer", "Sussex Spaniel", "Tibetan Mastiff", "Tibetan Terrier", "Toy Poodle", "Toy Terrier", "Vizsla", "Treeing Walker Coonhound", "Weimaraner", "Welsh Springer Spaniel", "West Highland White Terrier", "Whippet", "Wire Fox Terrier", "Yorkshire Terrier"]
+    
+    let wikipediaURL = "https://www.wikipedia.org/w/api.php"
+    var delegate: DogBreedManagerDelegate?
+    
+    let model = try? DogFullClassifier(configuration: MLModelConfiguration())
+    
+    func getPredictions(userPickedImage: UIImage) -> [DogBreed] {
+        let probs = predicttionsFromModel(userPickedImage)
+        let dogs = top2Prediction(probs)
+//        self.delegate?.updataDisplay(self, DogBreed: <#T##DogBreed#>)
+        return dogs
+    }
+    
+    func predicttionsFromModel(_ userPickedImage: UIImage) -> MLMultiArray {
+        // Ensure that we have a CGImage from the userPickedImage
+        guard let cgImage = userPickedImage.cgImage else {
+            fatalError("Failed to get CGImage from userPickedImage.")
+        }
+
+        // Create an input from the CGImage
+        guard let input = try? DogFullClassifierInput(input_1With: cgImage) else {
+            fatalError("Failed to create input for the model.")
+        }
+
+        // Make predictions using the model and the input
+        guard let output = try? model?.predictions(inputs: [input]) else {
+            fatalError("Failed to create output for the model.")
+        }
+
+        // Extract the probabilities from the output
+        let probs = output[0].Identity
+        return probs
+    }
+    
+    func top2Prediction(_ probs : MLMultiArray) -> [DogBreed] {
+        var dogBreeds:[DogBreed] = []
+        if let b = try? UnsafeBufferPointer<Float>(probs) {
+            let probsArray = Array(b)
+            let sortedProbsArray = probsArray.enumerated().sorted { $0.element > $1.element }
+            let top2 = sortedProbsArray.prefix(2)
+            for (index, value) in top2 {
+                let dogName = uniqueLabels[index]
+                if let dogInfo = requestWiki(dogName: dogName) {
+                    let dogDescription = dogInfo.description
+                    let dogImageURL = dogInfo.imageURL
+                    dogBreeds.append(DogBreed(name: dogName, probability: value, description: dogDescription, imageURL: dogImageURL))
+                } else {
+                    print("Error: failed to get dog information for \(dogName).")
+                }
+            }
+            return dogBreeds
+        }
+        return []
+    }
+    
+    func requestWiki(dogName: String) -> (description: String, imageURL: String)? {
+        
+        let parameters: [String:String] = [
+            "format": "json",
+            "action": "query",
+            "prop": "extracts|pageimages",
+            "exintro": "",
+            "explaintext": "",
+            "titles" : dogName,
+            "indexpageids": "",
+            "redirects": "1",
+            "pithumbsize" : "500"
+        ]
+        var dogDescription: String = ""
+        var dogImageURL: String = ""
+        
+        //To block the function until the closure has completed executing
+        let semaphore = DispatchSemaphore(value: 0)
+
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+//                print("Success got the dog info")
+//                print(response)
+                let dogJson: JSON = JSON(response.result.value!)
+                let pageid: String = dogJson["query"]["pageids"][0].stringValue
+                dogDescription = dogJson["query"]["pages"][pageid]["extract"].stringValue
+                dogImageURL = dogJson["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+            }
+            semaphore.signal()
+        }
+        
+        _ = semaphore.wait(timeout: .now() + 5.0)
+        if dogDescription.isEmpty || dogImageURL.isEmpty {
+            return nil
+        }
+        return (description: dogDescription, imageURL: dogImageURL)
+    }
+    
+    
+}
