c45d3705ab71092c3325d6bd7fa177d5a900e1e8
shiguoFeng <shiguo@ualberta.ca>
2023-05-17 20:41:35 -0600

    final

---
+++ Pods/Target Support Files/Pods-DogIdentifier-DogIdentifierUITests/Pods-DogIdentifier-DogIdentifierUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogIdentifier_DogIdentifierUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DogIdentifier_DogIdentifierUITests
+@end
---
+++ Pods/Target Support Files/Pods-DogIdentifier/Pods-DogIdentifier-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogIdentifier : NSObject
+@end
+@implementation PodsDummy_Pods_DogIdentifier
+@end
